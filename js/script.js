77d4de5d1fb3e1d4de6c95dc114c76e380302d53
ElisabettaMirti <belmirti@gmail.com>
2024-05-28 12:56:35 +0200

    possibilita' di modificare in tempo reale con un input sia l'h1 che l'url dell'immagine

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message:'Hello Vue!',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Ginger_european_cat.jpg/220px-Ginger_european_cat.jpg'
+            message:'',
+            image: ''
         }
     }
 }).mount('#app');
